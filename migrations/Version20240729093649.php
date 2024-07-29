06182dc23652dff917b4f62b69b72193ae84de82
Maël Gangloff <contact@maelgangloff.fr>
2024-07-29 11:37:52 +0200

    fix: make migration

--- migrations/Version20240729093649.php
+++ migrations/Version20240729093649.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240728213336 extends AbstractMigration
+final class Version20240729093649 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/Entity/WatchListTrigger.php
+++ src/Entity/WatchListTrigger.php
@@ -25,6 +25,9 @@ class WatchListTrigger
     #[Groups(['watchlist:list', 'watchlist:item', 'watchlist:create', 'watchlist:update'])]
     private ?TriggerAction $action = null;
 
+    #[ORM\ManyToOne(inversedBy: 'watchListTriggers')]
+    private ?Connector $connector = null;
+
     public function getEvent(): ?string
     {
         return $this->event;
@@ -57,6 +60,18 @@ class WatchListTrigger
     public function setAction(TriggerAction $action): static
     {
         $this->action = $action;
+
+        return $this;
+    }
+
+    public function getConnector(): ?Connector
+    {
+        return $this->connector;
+    }
+
+    public function setConnector(?Connector $connector): static
+    {
+        $this->connector = $connector;
 
         return $this;
     }
