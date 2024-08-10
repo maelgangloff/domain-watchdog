880d5b45752d6a75d2cc05bf3e7e8d8aaed82dba
Maël Gangloff <contact@maelgangloff.fr>
2024-08-10 15:44:16 +0200

    fix: process watchlists every day

--- src/Scheduler/SendNotifWatchListTriggerSchedule.php
+++ src/Scheduler/SendNotifWatchListTriggerSchedule.php
@@ -21,7 +21,7 @@ final readonly class SendNotifWatchListTriggerSchedule implements ScheduleProvid
     {
         return (new Schedule())
             ->add(
-                RecurringMessage::every('1 week', new ProcessWatchListsTrigger()),
+                RecurringMessage::every('1 day', new ProcessWatchListsTrigger()),
             )
             ->stateful($this->cache);
     }
