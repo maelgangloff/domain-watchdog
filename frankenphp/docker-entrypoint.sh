f34c1a5d7e808aa67b7f8cef4a9796f410cc3100
Maël Gangloff <contact@maelgangloff.fr>
2024-08-15 18:14:01 +0200

    fix: generate JWT keys on first launch

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -53,6 +53,8 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		fi
 	fi
 
+	php bin/console lexik:jwt:generate-keypair || true
+
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 fi
