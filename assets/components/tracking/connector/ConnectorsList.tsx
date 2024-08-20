92106a15a2d815cc9c3532c86ee4fa9b12b4c0a1
Maël Gangloff <contact@maelgangloff.fr>
2024-08-20 13:23:50 +0200

    fix: explicit error message on connector delete

--- assets/components/tracking/connector/ConnectorsList.tsx
+++ assets/components/tracking/connector/ConnectorsList.tsx
@@ -1,4 +1,4 @@
-import {Card, Divider, Popconfirm, theme, Typography} from "antd";
+import {Card, Divider, message, Popconfirm, theme, Typography} from "antd";
 import {t} from "ttag";
 import {DeleteFilled} from "@ant-design/icons";
 import React from "react";
@@ -11,17 +11,23 @@ export type ConnectorElement = Connector & { id: string, createdAt: string }
 
 export function ConnectorsList({connectors, onDelete}: { connectors: ConnectorElement[], onDelete: () => void }) {
     const {token} = useToken()
+    const [messageApi, contextHolder] = message.useMessage()
+
+    const onConnectorDelete = (connector: ConnectorElement) => deleteConnector(connector.id)
+        .then(onDelete)
+        .catch(() => messageApi.error(t`An error occurred while deleting the Connector. Make sure it is not used in any Watchlist`))
 
     return <>
         {connectors.map(connector =>
             <>
+                {contextHolder}
                 <Card hoverable title={<Typography.Text
                     title={new Date(connector.createdAt).toLocaleString()}>{t`Connector ${connector.provider}`}</Typography.Text>}
                       size='small'
                       style={{width: '100%'}}
                       extra={<Popconfirm title={t`Delete the Connector`}
                                          description={t`Are you sure to delete this Connector?`}
-                                         onConfirm={() => deleteConnector(connector.id).then(onDelete)}
+                                         onConfirm={() => onConnectorDelete(connector)}
                                          okText={t`Yes`}
                                          cancelText={t`No`}
                       ><DeleteFilled style={{color: token.colorError}}/></Popconfirm>}>
