2ac1ae1a4c258e38f7f1faef42803e45d8641752
Maël Gangloff <contact@maelgangloff.fr>
2024-08-21 16:32:23 +0200

    fix: return handle if no jCard

--- assets/utils/index.tsx
+++ assets/utils/index.tsx
@@ -69,6 +69,8 @@ export const actionToIcon = (a: EventAction) => a === 'registration' ?
 
 
 export const entityToName = (e: { entity: Entity }): string => {
+    if (e.entity.jCard.length === 0) return e.entity.handle
+
     const jCard = vCard.fromJSON(e.entity.jCard)
     let name = e.entity.handle
     if (jCard.data.fn && !Array.isArray(jCard.data.fn) && jCard.data.fn.valueOf() !== '') name = jCard.data.fn.valueOf()
