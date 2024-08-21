c58fbb47be451843dff4550f08127997ccedaaf6
Maël Gangloff <contact@maelgangloff.fr>
2024-08-21 02:18:14 +0200

    chore: remove event icon on Watchlist

--- assets/components/tracking/watchlist/WatchlistCard.tsx
+++ assets/components/tracking/watchlist/WatchlistCard.tsx
@@ -11,7 +11,7 @@ import {Connector} from "../../../utils/api/connectors";
 import useBreakpoint from "../../../hooks/useBreakpoint";
 import {CalendarWatchlistButton} from "./CalendarWatchlistButton";
 import {rdapEventDetailTranslation, rdapEventNameTranslation} from "../../search/rdapTranslation";
-import {actionToColor, actionToIcon} from "../../../utils";
+import {actionToColor} from "../../../utils";
 
 export function WatchlistCard({watchlist, onUpdateWatchlist, connectors, onDelete}: {
     watchlist: Watchlist,
@@ -78,7 +78,7 @@ export function WatchlistCard({watchlist, onUpdateWatchlist, connectors, onDelet
                     events: watchlist.triggers?.filter(t => t.action === 'email')
                         .map(t => <Tooltip
                                 title={t.event in rdapEventDetailTranslated ? rdapEventDetailTranslated[t.event as keyof typeof rdapEventDetailTranslated] : undefined}>
-                                <Tag color={actionToColor(t.event)} icon={actionToIcon(t.event)}>
+                                <Tag color={actionToColor(t.event)}>
                                     {rdapEventNameTranslated[t.event as keyof typeof rdapEventNameTranslated]}
                                 </Tag>
                             </Tooltip>
