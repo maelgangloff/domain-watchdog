4761c982adbadfe42175c6b1566448fd8c4a697d
Maël Gangloff <contact@maelgangloff.fr>
2024-12-29 01:06:23 +0100

    fix: is_numeric instead of is_int

--- src/Service/RDAPService.php
+++ src/Service/RDAPService.php
@@ -509,7 +509,7 @@ readonly class RDAPService
 
         $entity->setHandle($rdapEntity['handle']);
 
-        if (array_key_exists('remarks', $rdapEntity) && is_array($rdapEntity['remarks']) && !is_int($rdapEntity['handle'])) {
+        if (array_key_exists('remarks', $rdapEntity) && is_array($rdapEntity['remarks']) && !is_numeric($rdapEntity['handle'])) {
             $entity->setRemarks($rdapEntity['remarks']);
         }
 
