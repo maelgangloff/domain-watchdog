fd44b77cf0a4672f5d969164e17e3aaf6d819a1f
Maël Gangloff <contact@maelgangloff.fr>
2025-10-05 19:07:42 +0200

    ci: add deploy.yml

---
+++ deploy.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Config
+set('dotenv', '/var/www/domain-watchdog/.env.local');
+set('repository', 'https://github.com/maelgangloff/domain-watchdog.git');
+set('branch', 'demo-instance');
+set('keep_releases', 1);
+
+add('shared_files', []);
+add('shared_dirs', ['node_modules', 'public/content', 'config/jwt', 'config/app']);
+add('writable_dirs', []);
+
+// Hosts
+
+host('dw1.srv.domainwatchdog.eu')
+    ->setPort(2004)
+    ->set('remote_user', 'deploy')
+    ->set('deploy_path', '/var/www/demo.domainwatchdog.eu');
+
+desc('Build frontend');
+task('front:build', function () {
+    run("cd {{release_or_current_path}} && yarn install --no-dev && yarn run build && yarn run ttag:po2json && rm -rf node_modules");
+});
+
+desc('Generate JWT keys');
+task('jwt:generate', function () {
+    run("cd {{release_or_current_path}} && {{bin/console}} lexik:jwt:generate-keypair");
+});
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
+after('deploy:vendors', 'database:migrate');
+after('deploy:vendors', 'jwt:generate');
+after('deploy:vendors', 'front:build');
