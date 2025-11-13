e5a28514c5c9cc6c7580ffafe159707f79583cf5
Maël Gangloff <contact@maelgangloff.fr>
2025-11-13 09:26:29 +0100

    ci: exclude docs path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://domainwatchdog.eu',
     integrations: [
         starlight({
             title: 'Domain Watchdog',
